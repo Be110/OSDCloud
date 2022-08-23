d9bf116914b2491c6dc12c57e24bb3d9bd845c21
Be110 <96010785+Be110@users.noreply.github.com>
2022-08-23 11:32:10 +0200

    Update OSDCloud_and_Autopilot.ps1
    Change Grouptags

--- OSDCloud_and_Autopilot.ps1
+++ OSDCloud_and_Autopilot.ps1
@@ -5,15 +5,15 @@ Write-Host "=======================================================" -Foreground
 Write-Host "=================== Autopilot Join ====================" -ForegroundColor Yellow
 Write-Host "=======================================================" -ForegroundColor Yellow
 Write-Host "1: Group Tag CMD AutopilotJoin" -ForegroundColor Yellow
-Write-Host "2: Group Tag CMD AutopilotJoin" -ForegroundColor Yellow
-Write-Host "3: Group Tag CMD AutopilotJoin" -ForegroundColor Yellow
-Write-Host "4: Group Tag CMD AutopilotJoin" -ForegroundColor Yellow
-Write-Host "5: Group Tag CMD AutopilotJoin" -ForegroundColor Yellow
-Write-Host "6: Group Tag CMD AutopilotJoin" -ForegroundColor Yellow
-Write-Host "7: Group Tag CMD AutopilotJoin" -ForegroundColor Yellow
-Write-Host "8: Group Tag CMD AutopilotJoin" -ForegroundColor Yellow
-Write-Host "9: Group Tag CMD AutopilotJoin" -ForegroundColor Yellow
-Write-Host "10: Group Tag CMD AutopilotJoin" -ForegroundColor Yellow
+Write-Host "2: Group Tag CMDCZE AutopilotJoin" -ForegroundColor Yellow
+Write-Host "3: Group Tag CMMUDAUT AutopilotJoin" -ForegroundColor Yellow
+Write-Host "4: Group Tag CMMUDCHE AutopilotJoin" -ForegroundColor Yellow
+Write-Host "5: Group Tag CMMUDFRA AutopilotJoin" -ForegroundColor Yellow
+Write-Host "6: Group Tag CMMUDGER AutopilotJoin" -ForegroundColor Yellow
+Write-Host "7: Group Tag CMMUDITA AutopilotJoin" -ForegroundColor Yellow
+Write-Host "8: Group Tag CMMUDLUX AutopilotJoin" -ForegroundColor Yellow
+Write-Host "9: Group Tag CMMUDNLD AutopilotJoin" -ForegroundColor Yellow
+Write-Host "10: Group Tag CMMUDPOL AutopilotJoin" -ForegroundColor Yellow
 Write-Host "=======================================================" -ForegroundColor Yellow
 Write-Host "===================== OS Install ======================" -ForegroundColor Yellow
 Write-Host "=======================================================" -ForegroundColor Yellow
@@ -39,18 +39,18 @@ switch ($input)
 {
 	'0' {Exit}
 	'1' {Get-WindowsAutoPilotInfo.ps1 -Online -Assign -GroupTag "CMD"}
-	'2' {Get-WindowsAutoPilotInfo.ps1 -Online -Assign -GroupTag "CMD"}
-	'3' {Get-WindowsAutoPilotInfo.ps1 -Online -Assign -GroupTag "CMD"}
-	'4' {Get-WindowsAutoPilotInfo.ps1 -Online -Assign -GroupTag "CMD"}
-	'5' {Get-WindowsAutoPilotInfo.ps1 -Online -Assign -GroupTag "CMD"}
-	'6' {Get-WindowsAutoPilotInfo.ps1 -Online -Assign -GroupTag "CMD"}
-	'7' {Get-WindowsAutoPilotInfo.ps1 -Online -Assign -GroupTag "CMD"}
-	'8' {Get-WindowsAutoPilotInfo.ps1 -Online -Assign -GroupTag "CMD"}
-	'9' {Get-WindowsAutoPilotInfo.ps1 -Online -Assign -GroupTag "CMD"}
-    '10' {Get-WindowsAutoPilotInfo.ps1 -Online -Assign -GroupTag "CMD"}
-    '11' {Start-OSDCloud -OSLanguage de-de -OSBuild 21H2 -OSEdition Pro -ZTI}
-    '12' {Start-OSDCloud -OSLanguage en-us -OSBuild 21H2 -OSEdition Pro -ZTI}
-    '13' {Start-OSDCloud -OSLanguage cs-cz -OSBuild 21H2 -OSEdition Pro -ZTI} 
+	'2' {Get-WindowsAutoPilotInfo.ps1 -Online -Assign -GroupTag "CMDCZE"}
+	'3' {Get-WindowsAutoPilotInfo.ps1 -Online -Assign -GroupTag "CMMUDAUT"}
+	'4' {Get-WindowsAutoPilotInfo.ps1 -Online -Assign -GroupTag "CMMUDCHE"}
+	'5' {Get-WindowsAutoPilotInfo.ps1 -Online -Assign -GroupTag "CMMUDFRA"}
+	'6' {Get-WindowsAutoPilotInfo.ps1 -Online -Assign -GroupTag "CMMUDGER"}
+	'7' {Get-WindowsAutoPilotInfo.ps1 -Online -Assign -GroupTag "CMMUDITA"}
+	'8' {Get-WindowsAutoPilotInfo.ps1 -Online -Assign -GroupTag "CMMUDLUX"}
+	'9' {Get-WindowsAutoPilotInfo.ps1 -Online -Assign -GroupTag "CMMUDNLD"}
+    	'10' {Get-WindowsAutoPilotInfo.ps1 -Online -Assign -GroupTag "CMMUDPOL"}
+    	'11' {Start-OSDCloud -OSLanguage de-de -OSBuild 21H2 -OSEdition Pro -ZTI}
+    	'12' {Start-OSDCloud -OSLanguage en-us -OSBuild 21H2 -OSEdition Pro -ZTI}
+    	'13' {Start-OSDCloud -OSLanguage cs-cz -OSBuild 21H2 -OSEdition Pro -ZTI} 
 	'14' {Start-OSDCloudGU} 
 }
 
