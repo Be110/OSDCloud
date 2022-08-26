b03e25cf12700c8ec0f8ec775a42ea372468e38f
Be110 <96010785+Be110@users.noreply.github.com>
2022-08-26 14:20:43 +0200

    Update ZTI-OSDCloud.ps1

--- ZTI-OSDCloud.ps1
+++ ZTI-OSDCloud.ps1
@@ -2,9 +2,9 @@ Write-Host  -ForegroundColor Yellow "Starting Custom OSDCloud ..."
 cls
 Write-Host "===================== Main Menu =======================" -ForegroundColor Yellow
 Write-Host "=======================================================" -ForegroundColor Yellow
-Write-Host "1: Zero-Touch Win10 21H2 | Deutsch | Enterprise" -ForegroundColor Yellow
-Write-Host "2: Zero-Touch Win10 21H2 | English | Enterprise" -ForegroundColor Yellow
-Write-Host "3: Zero-Touch Win10 21H2 | Czech | Enterprise" -ForegroundColor Yellow
+Write-Host "1: Zero-Touch Win10 21H2 | Deutsch | Pro" -ForegroundColor Yellow
+Write-Host "2: Zero-Touch Win10 21H2 | English | Pro" -ForegroundColor Yellow
+Write-Host "3: Zero-Touch Win10 21H2 | Czech | Pro" -ForegroundColor Yellow
 Write-Host "4: Start GUI for custom OS"-ForegroundColor Yellow
 Write-Host "5: Exit`n"-ForegroundColor Yellow
 $input = Read-Host "Please make a selection"
@@ -20,7 +20,7 @@ switch ($input)
     '2' { Start-OSDCloud -OSLanguage en-us -OSBuild 21H2 -OSEdition Pro -ZTI }
     '3' { Start-OSDCloud -OSLanguage cs-cz -OSBuild 21H2 -OSEdition Pro -ZTI } 
     '4' { Start-OSDCloudGUI	} 
-    '5' { Exit		}
+    '5' { Exit	}
 }
 
 wpeutil reboot
