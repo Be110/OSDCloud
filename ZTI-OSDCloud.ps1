f8da367ae45727eba68826be2452fba2124004d5
Be110 <96010785+Be110@users.noreply.github.com>
2022-12-22 07:55:06 +0100

    Update ZTI-OSDCloud.ps1
    Update to W10 22H2

--- ZTI-OSDCloud.ps1
+++ ZTI-OSDCloud.ps1
@@ -3,11 +3,11 @@ cls
 Write-Host "===================== Main Menu =======================" -ForegroundColor Yellow
 Write-Host "=======================================================" -ForegroundColor Yellow
 Write-Host "1: Start GUI for custom OS"-ForegroundColor Yellow
-Write-Host "2: Zero-Touch Win10 21H1 | German | Pro" -ForegroundColor Yellow
-Write-Host "3: Zero-Touch Win10 21H1 | English | Pro" -ForegroundColor Yellow
-Write-Host "4: Zero-Touch Win10 21H1 | Czech | Pro" -ForegroundColor Yellow
-Write-Host "5: Zero-Touch Win10 21H1 | Polish | Pro" -ForegroundColor Yellow
-Write-Host "6: Zero-Touch Win10 21H1 | Italian | Pro" -ForegroundColor Yellow
+Write-Host "2: Zero-Touch Win10 22H2 | German | Pro" -ForegroundColor Yellow
+Write-Host "3: Zero-Touch Win10 22H2 | English | Pro" -ForegroundColor Yellow
+Write-Host "4: Zero-Touch Win10 22H2 | Czech | Pro" -ForegroundColor Yellow
+Write-Host "5: Zero-Touch Win10 22H2 | Polish | Pro" -ForegroundColor Yellow
+Write-Host "6: Zero-Touch Win10 22H2 | Italian | Pro" -ForegroundColor Yellow
 Write-Host "10: Exit`n"-ForegroundColor Yellow
 $input = Read-Host "Please make a selection"
 
@@ -19,11 +19,11 @@ Install-Module OSD -Force
 switch ($input)
 {
     '1' { Start-OSDCloudGUI	} 
-    '2' { Start-OSDCloud -OSLanguage de-de -OSVersion "Windows 10" -OSBuild 21H1 -OSEdition Pro -OSLicense Retail -ZTI }
-    '3' { Start-OSDCloud -OSLanguage en-us -OSVersion "Windows 10" -OSBuild 21H1 -OSEdition Pro -OSLicense Retail -ZTI }
-    '4' { Start-OSDCloud -OSLanguage cs-cz -OSVersion "Windows 10" -OSBuild 21H1 -OSEdition Pro -OSLicense Retail -ZTI } 
-    '5' { Start-OSDCloud -OSLanguage pl-pl -OSVersion "Windows 10" -OSBuild 21H1 -OSEdition Pro -OSLicense Retail -ZTI } 
-    '6' { Start-OSDCloud -OSLanguage it-it -OSVersion "Windows 10" -OSBuild 21H1 -OSEdition Pro -OSLicense Retail -ZTI } 
+    '2' { Start-OSDCloud -OSLanguage de-de -OSVersion "Windows 10" -OSBuild 22H2 -OSEdition Pro -OSLicense Retail -ZTI }
+    '3' { Start-OSDCloud -OSLanguage en-us -OSVersion "Windows 10" -OSBuild 22H2 -OSEdition Pro -OSLicense Retail -ZTI }
+    '4' { Start-OSDCloud -OSLanguage cs-cz -OSVersion "Windows 10" -OSBuild 22H2 -OSEdition Pro -OSLicense Retail -ZTI } 
+    '5' { Start-OSDCloud -OSLanguage pl-pl -OSVersion "Windows 10" -OSBuild 22H2 -OSEdition Pro -OSLicense Retail -ZTI } 
+    '6' { Start-OSDCloud -OSLanguage it-it -OSVersion "Windows 10" -OSBuild 22H2 -OSEdition Pro -OSLicense Retail -ZTI } 
     '10' { Exit	}
 }
 
