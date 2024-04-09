459f2a8867f67956368b1cf61e7cdd35bb2d7f85
Be110 <96010785+Be110@users.noreply.github.com>
2024-04-09 14:55:49 +0200

    Update ZTI-OSDCloud.ps1
    W11 Update to 23H2

--- ZTI-OSDCloud.ps1
+++ ZTI-OSDCloud.ps1
@@ -8,7 +8,7 @@ Write-Host "3: Zero-Touch Win10 22H2 | English | Pro" -ForegroundColor Yellow
 Write-Host "4: Zero-Touch Win10 22H2 | Czech | Pro" -ForegroundColor Yellow
 Write-Host "5: Zero-Touch Win10 22H2 | Polish | Pro" -ForegroundColor Yellow
 Write-Host "6: Zero-Touch Win10 22H2 | Italian | Pro" -ForegroundColor Yellow
-Write-Host "7: Zero-Touch Win11 22H2 | German | Pro" -ForegroundColor Yellow
+Write-Host "7: Zero-Touch Win11 23H2 | German | Pro" -ForegroundColor Yellow
 Write-Host "10: Exit`n"-ForegroundColor Yellow
 $input = Read-Host "Please make a selection"
 
@@ -25,7 +25,7 @@ switch ($input)
     '4' { Start-OSDCloud -OSLanguage cs-cz -OSVersion "Windows 10" -OSBuild 22H2 -OSEdition Pro -OSLicense Retail -ZTI } 
     '5' { Start-OSDCloud -OSLanguage pl-pl -OSVersion "Windows 10" -OSBuild 22H2 -OSEdition Pro -OSLicense Retail -ZTI } 
     '6' { Start-OSDCloud -OSLanguage it-it -OSVersion "Windows 10" -OSBuild 22H2 -OSEdition Pro -OSLicense Retail -ZTI } 
-    '7' { Start-OSDCloud -OSLanguage de-de -OSVersion "Windows 11" -OSBuild 22H2 -OSEdition Pro -OSLicense Retail -ZTI }
+    '7' { Start-OSDCloud -OSLanguage de-de -OSVersion "Windows 11" -OSBuild 23H2 -OSEdition Pro -OSLicense Retail -ZTI }
     '10' { Exit	}
 }
 
